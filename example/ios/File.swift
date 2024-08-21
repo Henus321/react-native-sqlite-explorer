5cfb25129bf0ae7581c240c13bb6482926a4f8fc
Alexander Erkhov <tyrantbud@yandex.ru>
2024-08-21 17:35:50 +0300

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  SqliteExplorerExample
+//
+
+import Foundation
---
+++ ios/SqliteExplorer.swift
@@ -0,0 +1,8 @@
+@objc(SqliteExplorer)
+class SqliteExplorer: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
